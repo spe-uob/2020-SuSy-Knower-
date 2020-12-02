d7ec6eab26d472864cbec9bdf6488680dc660314
kkadzielna <k.kadzielawa00@gmail.com>
2020-12-02 15:16:06 +0000

    the new app

---
+++ SuSy-Knower-/src/main/java/com/SuSyKnower/SuSyKnowerApplication.java
@@ -0,0 +1,13 @@
+package com.SuSyKnower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuSyKnowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuSyKnowerApplication.class, args);
+	}
+
+}
