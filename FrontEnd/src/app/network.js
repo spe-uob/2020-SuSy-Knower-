f62100ce8772d0f35a90c672d3b9740ccf4e21bd
JoelBearn <joelbearn@gmail.com>
2021-03-02 12:13:05 +0000

    fix makeParent
    When acting recursively the updated node is now passed.

--- FrontEnd/src/app/network.js
+++ FrontEnd/src/app/network.js
@@ -15,7 +15,7 @@
   var edges = new vis.DataSet([
     { from: 4, to: 1, id:"4-1"},
     { from: 3, to: 1, id:"3-1" },
-    { from: 1, to: 2, id:"1-2" },
+    { from: 2, to: 1, id:"2-1" },
     { from: 1, to: 5, id:"1-5" },
     { from: 5, to: 6, id:"6-1" },
     { from: 1, to: 7, id:"1-7" },
@@ -36,7 +36,12 @@
     nodes:{shape: "dot",
         size:nodeSize,
         borderWidth: nodeSize/4,
-        color:{border:"black",background:"white"},
+        color:{ border:"black",
+                background:"white",
+                highlight: {
+                    border: 'black',
+                    background: 'white'
+                  },},
         font:{face:"tahoma"},
         level:0,
         chosen:true,
@@ -56,8 +61,8 @@
             springLength: 200,
             springConstant: 0.01,
             nodeDistance: 200,
-            damping: 0.09,
-            avoidOverlap: 0
+            damping: 0.2,
+            avoidOverlap: 0.7
       }
     },
     layout:{
@@ -98,7 +103,6 @@ function setLevel(nodeId,Nlev){
 //logs levels of all nodes
 function displayLevels(){
     nodes.forEach(node =>{
-        //nodes.update({id:node.id,level:node.id})
         console.log(node.label+" is at level "+node.level)
     });
 }
@@ -107,11 +111,12 @@ function makeParentsAndChildren(node){
     var edgeIds = network.getConnectedEdges(node.id);
     edgeIds.forEach(edgeId => {
         var edge = edges.get(edgeId);
-        console.log("this is the edge id:"+edge.id)
         if(edge.from == node.id){
+            console.log("making PARENTS of:"+node.label)
             styleParent(edge,node.level);
         }
-        else{
+        else if(edge.to==node.id){
+            console.log("making CHILDREN of:"+node.label)
             styleChild(edge,node.level);
         }
     });
@@ -122,8 +127,8 @@ function makeParents(node){
     var edgeIds = network.getConnectedEdges(node.id);
     edgeIds.forEach(edgeId => {
         var edge = edges.get(edgeId);
-        console.log("this is the edge id:"+edge.id)
         if(edge.from == node.id){
+            console.log("making PARENTS of:"+node.label)
             styleParent(edge,node.level);
         }
     });
@@ -132,8 +137,8 @@ function makeChildren(node){
     var edgeIds = network.getConnectedEdges(node.id);
     edgeIds.forEach(edgeId => {
         var edge = edges.get(edgeId);
-        console.log("this is the edge id:"+edge.id)
         if(edge.to == node.id){
+            console.log("making CHILDREN of:"+node.label)
             styleChild(edge,node.level);
         }
     });
@@ -141,18 +146,18 @@ function makeChildren(node){
 
 
 function styleParent(edge,level){
-    console.log("making edge PARENT with TO:"+edge.to);
-    edges.update({id:edge.id,label:"POST"});
     node = nodes.get(edge.to);
+    console.log("making "+node.label+" a parent");
+    edges.update({id:edge.id,label:"POST"});
     nodes.update({id:node.id,level:level-1});
-    makeParents(node);
+    makeParents(nodes.get(node.id));
 }
 function styleChild(edge,level){
     console.log("making edge CHILD with FROM:"+edge.from);
     edges.update({id:edge.id,label:"PRE"});
     node = nodes.get(edge.from);
     nodes.update({id:node.id,level:level+1});
-    makeChildren(node);
+    makeChildren(nodes.get(node.id));
 }
 
 //on double click sets the selected node to be target node
@@ -166,11 +171,32 @@ network.on('doubleClick', function(params){
 network.on('click', function(params){
     selectedNodes = network.getSelectedNodes();
     selectedNodes.forEach(nodeId => {
-        console.log(network.getConnectedEdges(nodeId));
+        //console.log(network.getConnectedEdges(nodeId));
     });
 });
 
 nodes.forEach(node => {
     setLevel(node.id,0);
 });
+
+function FadeNode(fnode){
+    nodes.update({id:fnode.id,opacity:0.5});
+    nodes.forEach(node => {
+        console.log("opacity of "+node.label+" is: "+node.opacity);
+    });
+}
+function FadeEdge(fedge){
+    edges.update({id:fedge.id,opacity:0});
+}
+function FadeAll(){
+    nodes.forEach(node => {
+        FadeNode(node);
+    });
+    edges.forEach(edge =>{
+        FadeEdge(edge);
+    });
+}
+
+
 displayLevels();
+FadeAll();
