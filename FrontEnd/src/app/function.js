afc37f2e6c358015767a17a40641238f0ed84932
kkadzielna <k.kadzielawa00@gmail.com>
2021-03-28 11:04:26 +0100

    with frontend files, not integrated yet

---
+++ FrontEnd/src/app/function.js
@@ -0,0 +1,23 @@
+var keys = document.getElementById('myInput');
+var button = document.getElementById('sb');
+
+function searchfun() {
+  console.log(keys.value)
+  //change to filter for neatness and efficiency.
+  var found = false;
+  nodes.forEach(node => {
+    if(node.label.toLowerCase().includes(keys.value.toLowerCase())){
+      console.log("found!")
+      console.log(node)
+      found = true;
+      highlightNode(node);
+      //setTarget(node.id)
+    }
+  });
+  if(found == false){
+    console.log("not found!!")
+    //document.write(keys.value+'<b> No Such Course Found </b>')
+  }
+
+}
+button.addEventListener('click', searchfun)
