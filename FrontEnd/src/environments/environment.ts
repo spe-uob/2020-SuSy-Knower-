07efc351415e1ef7c545541c9768cc4fa8b74fe9
JoelBearn <joelbearn@gmail.com>
2021-05-14 14:24:01 +0100

    fixed reset bug

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_base_Url: 'http://ec2-34-224-31-136.compute-1.amazonaws.com:8080'
+  api_base_Url: 'http://ec2-3-90-191-26.compute-1.amazonaws.com:8080/'
 };
 
 /*
